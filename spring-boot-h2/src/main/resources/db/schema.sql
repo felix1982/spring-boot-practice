9de36ea217a266c44ca1c7dbb6ea66bde2f70460
felix1982 <felix_ada@163.com>
2019-02-22 22:33:55 +0800

    spring-boot-h2

---
+++ spring-boot-h2/src/main/resources/db/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE student(
+ id int not null,
+ name varchar(20) null,
+ age int null
+);
---
+++ spring-boot-multi-datasource/src/main/resources/db/data.sql
@@ -0,0 +1,2 @@
+INSERT  INTO  student VALUES (1,'张三',10);
+INSERT  INTO  student VALUES (2,'李四',16);
